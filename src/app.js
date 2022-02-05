dac7469fb1aac4242c4b25d47ef6f28ddd01038e
mehul555 <99032069+mehul555@users.noreply.github.com>
2022-02-05 20:29:11 +0530

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -4,3 +4,20 @@ document.getElementById("mail").addEventListener("click", (event) => {
 
   window.open("mailto:mehultalaviya323@gmail.com");
 });
+let i = 0;
+const bg = document.getElementById("container");
+setInterval(function () {
+  if (i == 0) {
+    bg.style.backgroundImage = "url(img/background.jpg)";
+    i++;
+  } else if (i == 1) {
+    bg.style.backgroundImage = "url(img/image1.jpg)";
+    i++;
+  } else if (i == 2) {
+    bg.style.backgroundImage = "url(img/image2.jpg)";
+    i++;
+  } else if (i == 3) {
+    bg.style.backgroundImage = "url(img/image3.jpg)";
+    i = 0;
+  }
+}, 3000);
