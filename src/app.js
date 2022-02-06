253754c5e9feccd7ffcc2d5cacf464c52ce9788a
mehul555 <99032069+mehul555@users.noreply.github.com>
2022-02-06 10:50:54 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,13 +11,13 @@ setInterval(function () {
     bg.style.backgroundImage = "url(img/background.jpg)";
     i++;
   } else if (i == 1) {
-    bg.style.backgroundImage = "url(img/image1.jpg)";
+    bg.style.backgroundImage = "url(img/image01.jpg)";
     i++;
   } else if (i == 2) {
-    bg.style.backgroundImage = "url(img/image2.jpg)";
+    bg.style.backgroundImage = "url(img/image02.jpg)";
     i++;
   } else if (i == 3) {
-    bg.style.backgroundImage = "url(img/image3.jpg)";
+    bg.style.backgroundImage = "url(img/image03.jpg)";
     i = 0;
   }
 }, 3000);
